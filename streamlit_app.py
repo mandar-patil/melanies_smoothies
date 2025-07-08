69159009aa8f09d59ed6e2dab43c72720aa76b72
mandar-patil <31374814+mandar-patil@users.noreply.github.com>
2025-07-08 20:45:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 # Import python packages
 import streamlit as st
+import requests
 #from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
@@ -23,15 +24,17 @@ if ingredients_list:
  st.write(ingredients_list)
  st.text(ingredients_list)
  ingredients_string = ''
+ 
 
  for fruit_chosen in ingredients_list:
-     ingredients_string += fruit_chosen + ' '
+      ingredients_string += fruit_chosen + ' '
+      smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+      sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+   
  st.write(ingredients_string)
 
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-st.text(smoothiefroot_response.json())
-sf_df = st.dataframe(data=smoothiefroot_response.json(),use_container_width=True)
+
+
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 time_to_insert = st.button('Submit Order')
