85a37d25fefa5c1da318ae85f389f3e91eb99a4c
mandar-patil <31374814+mandar-patil@users.noreply.github.com>
2025-07-08 20:38:02 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -35,6 +35,7 @@ st.text(smoothiefroot_response.json())
 my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ingredients_string + """','""" + name_on_order + """')"""
 time_to_insert = st.button('Submit Order')
- if time_to_insert:
-    session.sql(my_insert_stmt).collect()
-    st.success('Your Smoothie is ordered!', icon="✅")
+
+if time_to_insert:
+  session.sql(my_insert_stmt).collect()
+  st.success('Your Smoothie is ordered!', icon="✅")
